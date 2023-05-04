a22f8cb57a49d61bc9e7f1d6f16b643cad6b3361
eeera <eeera@Saranya.ht.home>
2023-05-03 22:04:34 -0400

    initial code changes has been completed

---
+++ Gitproject1/src/Gitdemo/newgit.java
@@ -0,0 +1,11 @@
+package Gitdemo;
+
+public class newgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.print("this is my first git project");
+			}
+
+}
---
+++ Gitproject1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author eeera
+ *
+ */
+module Gitproject1 {
+}
